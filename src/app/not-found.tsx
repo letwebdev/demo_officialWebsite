fc8c635977b0f91e684fb5bbaf027f3e1c3490f2
let webDeveloper <letwebdev@qq.com>
2024-06-08 14:52:03 +0800

    add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <section className="flex items-center justify-center flex-col h-[70vh]">
+      <h2>Not Found or not implemented yet</h2>
+      <Link
+        href="/"
+        className="text-xl"
+      >
+        Return Home
+      </Link>
+    </section>
+  )
+}
