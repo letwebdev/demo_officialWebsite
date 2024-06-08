a9fc52b411ecfae7211e2fec769654ea65e2d269
let webDeveloper <letwebdev@qq.com>
2024-06-08 13:23:11 +0800

    index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 "use client"
 
-import { default as Home1 } from "@/app/demo1/page"
-import { default as Home2 } from "@/app/demo2/page"
-// TODO Router
-Home1
-Home2
+const navTabs = [
+  {
+    label: "demo1",
+    href: "./demo1",
+  },
+  {
+    label: "demo2",
+    href: "./demo2",
+  },
+] as const
 
 export default function Index() {
-  return <main></main>
+  const [value, setValue] = React.useState<string>("")
+  const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue)
+  }
+  return (
+    <main>
+      <nav className="flex flex-col items-center justify-center h-[70vh]">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+        >
+          {navTabs.map((tab) => (
+            <Tab
+              className="text-2xl"
+              key={tab.label}
+              label={tab.label}
+              value={tab.href}
+              href={`demo_officialwebsite/${tab.href}`}
+              component={Link}
+            />
+          ))}
+        </Tabs>
+      </nav>
+    </main>
+  )
 }
