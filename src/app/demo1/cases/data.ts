2d1ed4754cf1b26ea18c10beccb9e678b4cf608a
let webDeveloper <letwebdev@qq.com>
2024-06-08 16:36:17 +0800

    UI

---
+++ src/app/demo1/cases/data.ts
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    image: "/demo_officialWebsite/favicon.ico",
+    title: "Title A",
+  },
+  {
+    image: "/demo_officialWebsite/favicon.ico",
+    title: "Title B",
+  },
+  {
+    image: "/demo_officialWebsite/favicon.ico",
+    title: "Title C",
+  },
+  {
+    image: "/demo_officialWebsite/favicon.ico",
+    title: "Title D",
+  },
+  {
+    image: "/demo_officialWebsite/favicon.ico",
+    title: "Title E",
+  },
+  {
+    image: "/demo_officialWebsite/favicon.ico",
+    title: "Title F",
+  },
+]
